7716cc8c77b790b55a1e87738f2592c7db0c68b2
bearcattt <ewigglet8@gmail.com>
2025-05-14 11:45:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,18 @@
-const WebSocket = require('ws');
-const http = require('http');
 const express = require('express');
-const path = require('path');
+const http = require('http');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
+app.get('/', (req, res) => {
+  res.send('Webhook Server Running');
+});
 
-  ws.on('message', (message) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
+app.post('/webhook', (req, res) => {
+  console.log('Received webhook:', req.body);
+  res.status(200).send('Webhook received');
 });
 
 server.listen(3000, () => {
